da97908cd38f13611191fd708a9037b18458f8b0
misop <michal.piovarci@gmail.com>
2021-02-07 22:27:29 +0100

    initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    // all custom jQuery will go here
+    $('#stylized_fab_toggle').click(function(){
+        $("#stylized_fab").toggle();
+    });
+    $('#percept_fab_toggle').click(function(){
+        $("#percept_fab").toggle();
+    });
+    $('#robogami_toggle').click(function(){
+        $("#robogami").toggle();
+    });
+    $('#surfcuit_toggle').click(function(){
+        $("#surfcuit").toggle();
+    });
+});
