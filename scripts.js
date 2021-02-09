14e8a6056ea2c6b0d4d3516c4ed69a4944cccc80
misop <michal.piovarci@gmail.com>
2021-02-09 22:40:18 +0100

    adjustments

--- scripts.js
+++ scripts.js
@@ -13,3 +13,36 @@ $(document).ready(function() {
         $("#surfcuit").toggle();
     });
 });
+
+function myFunction(selectObject) {
+    var value = selectObject.value;
+    var dEU = 'inline';
+    var dUS = 'none';
+    var dA = 'none';
+    switch(value) {
+        case '1':
+            dEU = 'none';
+            dUS = 'inline';
+            break;
+        case '2':
+            dEU = 'none';
+            dA = 'inline';
+            break;
+    }
+    var eus = document.getElementsByClassName('t_EU');
+    for(i = 0; i < eus.length; i++) {
+        eus[i].style.display = dEU;
+    }
+    var uss = document.getElementsByClassName('t_US');
+    for(i = 0; i < uss.length; i++) {
+        uss[i].style.display = dUS;
+    }
+    var as = document.getElementsByClassName('t_A');
+    for(i = 0; i < as.length; i++) {
+        as[i].style.display = dA;
+    }
+    var sels = document.getElementsByClassName('selector');
+    for(i = 0; i < sels.length; i++) {
+        sels[i].value = value;
+    }
+}
